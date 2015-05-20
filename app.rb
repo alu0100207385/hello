aa0d453776440fa06482d3edd013f39d62d61cf3
Aaron Socas Gaspar <alu0100207385@ull.edu.es>
2015-05-20 12:16:44 +0100

    prueba postgres

--- app.rb
+++ app.rb
@@ -14,7 +14,8 @@ class MyApp < Sinatra::Base
    	configure :production do
    		#DataMapper.setup(:default, ENV['DATABASE_URL'] || || 'postgres://#{Dir.pwd}/people.db')
    		#DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-   		DataMapper.setup(:default, ENV['OPENSHIFT_POSTGRESQL_DB_URL'] || ENV['HEROKU_POSTGRESQL_ONYX_URL'])
+   		#DataMapper.setup(:default, ENV['OPENSHIFT_POSTGRESQL_DB_URL'] || ENV['HEROKU_POSTGRESQL_ONYX_URL'])
+   		DataMapper.setup(:default, "postgresql://adminmkzy2cb:4sRv3TSzZ4eE@127.6.239.2:5432")
    	end
 
 	DataMapper::Logger.new($stdout, :debug)
